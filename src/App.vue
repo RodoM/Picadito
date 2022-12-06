e5cbeac53e3565a3fc2bbc08e168ff27c328d7e2
RodoM <rodolfo.meroi@web-experto.com.ar>
2022-12-06 18:42:12 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HomeView/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HomeView from '@/views/HomeView.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    HomeView
+  },
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
