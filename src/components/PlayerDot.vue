ab189a847b2d87c5987b6be055689977214836f7
RodoM <rodolfo.meroi@web-experto.com.ar>
2022-12-08 15:55:46 -0300

    update

--- src/components/PlayerDot.vue
+++ src/components/PlayerDot.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="flex flex-col items-center justify-center">
-    <div class="w-5 h-5 bg-white rounded-full md:w-8 md:h-8"></div>
-    <span class="font-semibold text-white text-xs md:text-base md:min-w-[6rem] min-h-[1.5rem] text-center">{{playerName}}</span>
+    <div class="relative w-5 h-5 bg-white rounded-full md:w-8 md:h-8">
+      <span class="absolute font-semibold transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">{{player.id}}</span>
+    </div>
+    <span class="font-semibold text-white text-xs md:text-base md:min-w-[6rem] min-h-[1.5rem] text-center">{{player.playerName}}</span>
   </div>
 </template>
 
@@ -9,7 +11,7 @@
 export default {
   name: 'PlayerDot',
   props: {
-    playerName: undefined,
+    player: undefined,
   },
 }
 </script>
