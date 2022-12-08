a6e086a752292c957110284cc94788c34541fc8d
RodoM <rodolfo.meroi@web-experto.com.ar>
2022-12-08 19:20:29 -0300

    fix name width

--- src/components/PlayerDot.vue
+++ src/components/PlayerDot.vue
@@ -3,7 +3,7 @@
     <div class="relative w-5 h-5 bg-white rounded-full md:w-8 md:h-8">
       <span class="absolute font-semibold transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">{{player.id}}</span>
     </div>
-    <span class="font-semibold text-white text-xs md:text-base md:min-w-[6rem] min-h-[1.5rem] text-center">{{player.playerName}}</span>
+    <span class="font-semibold text-white text-xs md:text-base min-w-[5rem] min-h-[1.5rem] text-center">{{player.playerName}}</span>
   </div>
 </template>
 
